cdb9f91bc8f80b13b56b1c9a362e8a6fc181521a
Aldo Villegas <avgaccs@gmail.com>
2025-02-16 18:11:54 -0800

    final files
    for some reason it shows as having a bunch of extra code that was supposed to already be uploaded

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 module.exports.getUsers = (req, res) => {
@@ -13,6 +14,33 @@ module.exports.getUsers = (req, res) => {
     .catch((err) => {
       if (err.status === 404) {
         res.status(404).send({ message: "no existe tal usuario" });
+      } else {
+        res.status(500).send({ message: "Error", err, body: req.body });
+      }
+    });
+};
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email })
+    .orFail(() => {
+      const error = new Error("No se ha encontrado ningún usuario");
+      error.status = 404;
+      throw error;
+    })
+    .then((data) => {
+      res.send({ status: true, data: data });
+      return bcrypt.compare(password, data.password);
+    })
+    .then((match) => {
+      if (!match) {
+        return Promise.reject(new Error("password o email incorrectos"));
+      }
+      res.send({ message: "¡Bienvenido de vuelta!" });
+    })
+    .catch((err) => {
+      if (err.status === 404) {
+        res.status(404).send({ message: "no se encontró tal usuario" });
       } else {
         res.status(500).send({ message: "Error", err, body: req.body });
       }
@@ -39,10 +67,15 @@ module.exports.getSpecificUser = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar })
+  const { name, about, avatar, email } = req.body;
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => User.create({ name, about, avatar, email, password: hash }))
     .then((user) => {
-      res.send({ data: user });
+      res.send({
+        email: user.email,
+        _id: user._id,
+      });
     })
     .catch((err) => {
       res.status(500).send({ message: "Error", err, body: req.body });
--- models/user.js
+++ models/user.js
@@ -20,6 +20,34 @@ const userSchema = new mongoose.Schema({
         require_protocol: true,
       }),
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Incorrect email or password"));
+    }
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+
+      return user;
+    });
+  });
+};
 
 module.exports = mongoose.model("user", userSchema);
